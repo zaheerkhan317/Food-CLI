8b0fb1a5255ceb201d77f964be52b1f4a8778b72
zaheerkhan317 <gousezahir100@gmail.com>
2024-08-03 02:18:31 +0530

    feat: Add Customer Exists Exception

---
+++ src/com/foodiecliapp/exceptions/CustomerExistsException.java
@@ -0,0 +1,8 @@
+package com.foodiecliapp.exceptions;
+
+public class CustomerExistsException extends Exception{
+
+    public CustomerExistsException(String message){
+        super(message);
+    }
+}
