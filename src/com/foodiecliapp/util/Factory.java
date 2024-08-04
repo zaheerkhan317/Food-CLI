c36edb719e896f54ac0546db910083bc8dc15a00
zaheerkhan317 <gousezahir100@gmail.com>
2024-08-04 17:02:29 +0530

    feat: Updated Factory for Dishes

--- src/com/foodiecliapp/util/Factory.java
+++ src/com/foodiecliapp/util/Factory.java
@@ -1,10 +1,17 @@
 package com.foodiecliapp.util;
 
 import com.foodiecliapp.controller.CustomerController;
+import com.foodiecliapp.controller.DishController;
 import com.foodiecliapp.repository.CustomerRepository;
+import com.foodiecliapp.repository.DishRepository;
 import com.foodiecliapp.service.CustomerServiceImpl;
+import com.foodiecliapp.service.DishServiceImpl;
 
 public class Factory {
+
+    public static CsvReader getCsvReader(){
+        return new CsvReader();
+    }
 
     public static CustomerRepository getCustomerRepository(){
         return new CustomerRepository();
@@ -16,5 +23,17 @@ public class Factory {
 
     public static CustomerController getCustomerController(){
         return new CustomerController(getCustomerServiceImpl());
+    }
+
+    public static DishRepository getDishRepository(){
+        return new DishRepository();
+    }
+
+    public static DishServiceImpl getDishService(){
+        return new DishServiceImpl(getDishRepository());
+    }
+
+    public static DishController getDishController(){
+        return new DishController(getDishService());
     }
 }
