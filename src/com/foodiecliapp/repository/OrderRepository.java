6e0106d12779872d0556e088fa7941931dc36977
zaheerkhan317 <gousezahir100@gmail.com>
2024-08-04 19:36:33 +0530

    feat: Updated Restaurant Repository

---
+++ src/com/foodiecliapp/repository/OrderRepository.java
@@ -0,0 +1,29 @@
+package com.foodiecliapp.repository;
+
+import com.foodiecliapp.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class OrderRepository {
+
+    private List<Order> orderList;
+
+    public OrderRepository(){
+        this.orderList = new ArrayList<>();
+    }
+
+    public Order save(Order order){
+        this.orderList.add(order);
+        return order;
+    }
+
+    public List<Order> getOrderList(){
+        return this.orderList;
+    }
+
+    public Optional<Order> findOrderById(String id){
+        return this.orderList.stream().filter(order -> order.getId().equals(id)).findFirst();
+    }
+}
